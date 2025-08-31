eebc4d1799ff74e17664864112c43848624504f6
YagiGD <its.charbs@gmail.com>
2025-08-31 13:21:16 -0400

    Update score.js

--- js/score.js
+++ js/score.js
@@ -11,10 +11,10 @@ const scale = 3;
  * @returns {Number}
  */
 export function score(rank, percent, minPercent) {
-    if (rank > 1500) {
+    if (rank > 2500) {
         return 0;
     }
-    if (rank > 750 && percent < 100) {
+    if (rank > 1250 && percent < 100) {
         return 0;
     }
 
