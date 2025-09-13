8208bd8e7965f51bb644e6c0dd74a0042117d30f
YagiGD <its.charbs@gmail.com>
2025-09-13 10:02:26 -0400

    Update Roulette.js

--- js/pages/Roulette.js
+++ js/pages/Roulette.js
@@ -168,7 +168,7 @@ export default {
                 return;
             }
 
-            if (!this.useMainList && !this.useExtendedList) {
+            if (!this.useMainList && !this.useExtendedList && !this.useGlobalList) {
                 return;
             }
 
