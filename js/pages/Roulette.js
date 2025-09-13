8d9ca553ae376608ccc66657a619c5d3c078a845
YagiGD <its.charbs@gmail.com>
2025-09-13 09:59:33 -0400

    Update Roulette.js

--- js/pages/Roulette.js
+++ js/pages/Roulette.js
@@ -24,6 +24,10 @@ export default {
                         <input type="checkbox" id="extended" value="Extended List" v-model="useExtendedList">
                         <label for="extended">Extended List</label>
                     </div>
+                    <div class="check">
+                        <input type="checkbox" id="global" value="Global List" v-model="useGlobalList">
+                        <label for="extended">Extended List</label>
+                    </div>
                     <Btn @click.native.prevent="onStart">{{ levels.length === 0 ? 'Start' : 'Restart'}}</Btn>
                 </form>
                 <p class="type-label-md" style="color: #aaa">
