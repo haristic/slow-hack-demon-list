372ea89e217a9d46ecfcd08f37dd1e625c2c392f
YagiGD <its.charbs@gmail.com>
2025-09-14 13:43:37 -0400

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -38,10 +38,6 @@ export default {
                         </td>
                     </tr>
                 </table>
-                <p v-if="list && list.length > 0 && filteredListDisplay && filteredListDisplay.length === 0" class="no-results type-body-lg">
-                No levels found matching your search.
-                </p>
-            </div>
 
             <script>
 export default {
