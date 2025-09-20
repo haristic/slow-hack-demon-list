eee6ce09ca3353011fbd5b24ca4500b82d289d9c
YagiGD <its.charbs@gmail.com>
2025-09-20 18:00:35 -0400

    Update routes.js

--- js/routes.js
+++ js/routes.js
@@ -1,4 +1,5 @@
 import List from './pages/List.js';
+import Platformers from './pages/Platformers.js';
 import Leaderboard from './pages/Leaderboard.js';
 import Roulette from './pages/Roulette.js';
 
