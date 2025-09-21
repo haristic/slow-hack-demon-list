b01c0b28d18fbb5ee12adfb89550ba46b96c87b7
YagiGD <its.charbs@gmail.com>
2025-09-21 12:45:46 -0400

    Update Leaderboard.js

--- js/pages/Leaderboard.js
+++ js/pages/Leaderboard.js
@@ -1,4 +1,5 @@
 import { fetchLeaderboard } from '../content.js';
+import { fetchLeaderboard } from '../content plat.js';
 import { localize } from '../util.js';
 
 import Spinner from '../components/Spinner.js';
