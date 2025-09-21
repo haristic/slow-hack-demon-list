cc06ca0e3e462c23fe57466c689e76d3514104e8
YagiGD <its.charbs@gmail.com>
2025-09-21 17:59:48 -0400

    Update routes.js

--- js/routes.js
+++ js/routes.js
@@ -1,11 +1,9 @@
 import List from './pages/List.js';
-import Platformers from './pages/Platformers.js';
 import Leaderboard from './pages/Leaderboard.js';
 import Roulette from './pages/Roulette.js';
 
 export default [
     { path: '/', component: List },
-    { path: '/platformers', component: Platformers },
     { path: '/leaderboard', component: Leaderboard },
     { path: '/roulette', component: Roulette },
 ];
