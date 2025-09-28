ab17880c936961168ff7e62cd6978108283c5fd1
YagiGD <its.charbs@gmail.com>
2025-09-28 13:31:02 -0400

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -47,10 +47,6 @@ export default {
                             <p>{{ score(selected + 1, 100, level.percentToQualify) }}</p>
                         </li>
                         <li>
-                            <div class="type-title-sm">ID</div>
-                            <p>{{ level.id }}</p>
-                        </li>
-                        <li>
                             <div class="type-title-sm">Password</div>
                             <p>{{ level.password || 'Free to Copy' }}</p>
                         </li>
