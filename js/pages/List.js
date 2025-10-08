fa0d7dd9d3c7ab1881eae209e8058e46423c9ec2
YagiGD <its.charbs@gmail.com>
2025-10-08 11:47:07 -0400

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -22,10 +22,6 @@ export default {
         </main>
         <main v-else class="page-list">
             <div class="list-container">
-            
-            <div class="search-wrapper">
-        <input type="text" placeholder="Search by level name..." class="search-input type-body-lg">
-        
                 <table class="list" v-if="list">
                     <tr v-for="([level, err], i) in list">
                         <td class="rank">
