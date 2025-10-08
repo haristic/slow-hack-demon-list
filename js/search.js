a02f67a61e59f12492dc7964976dd6fb3db55686
YagiGD <its.charbs@gmail.com>
2025-10-08 13:24:29 -0400

    Update search.js

--- js/search.js
+++ js/search.js
@@ -1,5 +1,5 @@
 const searchInput = document.getElementById('search');
-const listItems = document.querySelectorAll('#_list li');
+const listItems = document.data('#_list li');
 
 searchInput.data('input', function() {
   const query = this.value.toLowerCase();
