f65df44962ee7bf5f4dd1edb02bb3fbaab648b76
YagiGD <its.charbs@gmail.com>
2025-10-08 12:12:56 -0400

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -26,7 +26,7 @@ export default {
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="viewport" content="width=device-width, initial-scale=0.1">
   <title>Search Bar Example</title>
 </head>
 <body>
