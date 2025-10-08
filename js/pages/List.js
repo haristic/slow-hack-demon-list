59b1b87f0fcd00456a7f34db828053807cad88ba
YagiGD <its.charbs@gmail.com>
2025-10-08 15:38:20 -0400

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -33,7 +33,7 @@ export default {
                     />
                 </div>
             
-                <table class="list" v-if="filteredList">
+                <table class="list" v-if="filteredList.length">
                     <tr v-for="([level, err], i) in filteredList">
                         <td class="rank">
                             <p v-if="i + 1 <= 150" class="type-label-lg">#{{ i + 1 }}</p>
