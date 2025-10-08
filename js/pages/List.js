4be9b747a24446a84f276931e7970b5d73551883
YagiGD <its.charbs@gmail.com>
2025-10-08 13:15:00 -0400

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -27,6 +27,11 @@ export default {
     <input type="text" id="search" placeholder="Search..." />
   </div>
   <script src="search.js"></script>
+
+  <ul id="_list">
+  <li>oblivious</li>
+  <li>flighter</li>
+</ul>
             
                 <table class="list" v-if="list">
                     <tr v-for="([level, err], i) in list">
