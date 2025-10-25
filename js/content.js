a36d01e9e1f10a24660f5d275c78f90b305b24a6
YagiGD <its.charbs@gmail.com>
2025-10-25 16:27:39 -0400

    Update content.js

--- js/content.js
+++ js/content.js
@@ -4,6 +4,7 @@ import { round, score } from './score.js';
  * Path to directory containing `_list.json` and all levels
  */
 const dir = '/data';
+const dir = '/data2';
 
 export async function fetchList() {
     const listResult = await fetch(`${dir}/_list.json`);
