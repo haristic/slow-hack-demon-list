e6f114396a11c7d73495d185592c8a8abef9a083
YagiGD <its.charbs@gmail.com>
2025-10-27 17:32:04 -0400

    Update score.js

--- js/score.js
+++ js/score.js
@@ -24,7 +24,7 @@ export function score(rank, percent, minPercent) {
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
     */
     // New formula
-    let score = (-1*Math.pow(rank-0, 1) + 26) *
+    let score = (-100*Math.pow(rank-0, 1) + 26) *
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
 
     score = Math.max(0, score);
