34e911d599265dcff970635505672f59c8a77d2a
YagiGD <its.charbs@gmail.com>
2025-11-30 09:44:39 -0500

    Update routes.js

--- js/routes.js
+++ js/routes.js
@@ -1,7 +1,5 @@
-import List from './pages/List.js';
 import Leaderboard from './pages/Leaderboard.js';
 
 export default [
-    { path: '/', component: List },
     { path: '/leaderboard', component: Leaderboard },
 ];
